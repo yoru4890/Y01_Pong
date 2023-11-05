5f26026ec8b442be2fe4a39160e9713fab06dbb2
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-11-06 01:31:58 +0900

    [FIX] MAP (코드정리)

--- Pong/GameManager.cpp
+++ Pong/GameManager.cpp
@@ -6,12 +6,24 @@ HRESULT GameManager::Initialize(HINSTANCE hInstance, LPCWSTR title, UINT width, 
     hr = D2DFramework::Initialize(hInstance, title, width, height);
     ThrowIfFailed(hr, "Failed in D2DFramework::Initailize()");
 
-    // 파일 읽기
-    for (float x = 100, y = 75; x <= 924; x++) { mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, y)); }
-    for (float x = 100, y = 75; y <= 693; y++) { mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, y)); }
-    for (float x = 924, y = 75; y <= 693; y++) { mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, y)); }
-    for (float x = 100, y = 693; x <= 924; x++) { mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, y)); }
-    for (float x = 512, y = 75; y <= 687; y += 33) { mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, y)); }
+    const D2D_POINT_2F WALL_TOP_LEFT({ 100.0f,75.0f }), WALL_BOTTOM_RIGHT({ 924.0f,693.0f });
+    
+    for (float x = WALL_TOP_LEFT.x; x <= WALL_BOTTOM_RIGHT.x; x++)
+    {
+        mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, WALL_TOP_LEFT.y));
+        mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", x, WALL_BOTTOM_RIGHT.y));
+    }
+    
+    for (float y = WALL_TOP_LEFT.y; y <= WALL_BOTTOM_RIGHT.y; y++)
+    {
+        mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", WALL_TOP_LEFT.x, y));
+        mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", WALL_BOTTOM_RIGHT.x, y));
+    }
+
+    for (float y = WALL_TOP_LEFT.y; y <= WALL_BOTTOM_RIGHT.y; y += 32.55f)
+    {
+        mWalls.push_back(std::make_shared<Actor>(this, L"Images/wall.png", (WALL_TOP_LEFT.x + WALL_BOTTOM_RIGHT.x)/2, y));
+    }
 
     return S_OK;
 }
@@ -20,7 +32,7 @@ void GameManager::Render()
 {
     mspRenderTarget->BeginDraw();
     mspRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
-    mspRenderTarget->Clear(D2D1::ColorF(0.0f, 0.2f, 0.4f, 1.0f));
+    mspRenderTarget->Clear(D2D1::ColorF(0.0f, 0.0f, 0.0f, 1.0f));
 
     for (auto& wall : mWalls)
     {
